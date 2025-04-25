1d3856d92c1716c53bc177fe74908d786262326d
hideto <hideto_shimoie.23@icloud.com>
2025-04-25 15:16:24 +0900

    初期デザイン削除、ヘッダーフッター編集、アイコン編集

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,9 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -15,9 +12,8 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
+        headerShown: true, 
+        headerTitleAlign: 'center',
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
@@ -29,15 +25,27 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'ホーム',
+          tabBarIcon: ({ color }) => <FontAwesome name="home" size={24} color={color} />,
+          headerStyle: {
+            backgroundColor: '#8BDAFF', // ヘッダーの背景色を設定
+          },
+          headerTitleStyle: {
+            color: 'white', // ヘッダーのテキストの色を白に設定
+          },
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="setting"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: '設定',
+          tabBarIcon: ({ color }) => <FontAwesome name="pencil-square-o" size={24} color={color} />,
+          headerStyle: {
+            backgroundColor: '#8BDAFF', // ヘッダーの背景色を設定
+          },
+          headerTitleStyle: {
+            color: 'white', // ヘッダーのテキストの色を白に設定
+          },
         }}
       />
     </Tabs>
