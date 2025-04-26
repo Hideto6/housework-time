05106038a3781a561dfdef4c2f90dc8ed42b4a38
hideto <hideto_shimoie.23@icloud.com>
2025-04-26 22:55:06 +0900

    設定画面デザイン基盤作成

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -1,18 +1,42 @@
-import { StyleSheet, View,Text} from 'react-native';
+import { StyleSheet, View,Text,SafeAreaView} from 'react-native';
 
 export default function TabTwoScreen() {
   return (
- <View style={styles.container}>
-      <Text>は</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.houseworkSettingContainer}>
+        <Text style={styles.titleText}>家事の設定</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'white',
+    justifyContent: 'flex-start',
+    backgroundColor: 'white',
+    paddingTop: 15,
+  },
+  houseworkSettingContainer: {
+    width: 330,
+    paddingVertical: 20,
+    borderWidth:1,
+    borderColor:'#EEEEEE',
+    borderRadius: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,  //影の濃さ
+    shadowRadius: 4,
+    elevation: 3,  //android用
+  },
+  titleText:{
+    fontSize: 20,
+    fontFamily: 'Arial',
+    fontWeight: 'bold',
   },
 });
