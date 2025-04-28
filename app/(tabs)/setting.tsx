5081fca0e62b70692115d78a652b4029b86240ca
hideto <hideto_shimoie.23@icloud.com>
2025-04-28 23:15:44 +0900

    フラットリストでsort関数使用

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -152,7 +152,18 @@ export default function TabTwoScreen() {
           <Text style={styles.noTaskText}>予定がありません</Text>
         ) : (
           <FlatList
-            data={houseworkList}
+            data={[...houseworkList].sort((a, b) => {
+              const weekOrder = {
+                '月曜日': 0,
+                '火曜日': 1,
+                '水曜日': 2,
+                '木曜日': 3,
+                '金曜日': 4,
+                '土曜日': 5,
+                '日曜日': 6,
+              };
+              return weekOrder[a.week] - weekOrder[b.week];
+            })}
             renderItem={renderItem}
             keyExtractor={(item, index) => index.toString()} // インデックスをキーとして使用
             contentContainerStyle={{ paddingTop: 20 }}
